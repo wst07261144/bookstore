b984121236fd399f521bc7f6a0eac561e0c29939
wst07261144 <wst07261144@gmail.com>
2019-03-23 22:42:29 +0800

    Add book list

--- books/urls.py
+++ books/urls.py
@@ -7,5 +7,7 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # TODO books_id
     url(r'books/(?P<books_id>\d+)/$', views.detail, name='detail'), # 详情页
+    url(r'^list/(?P<type_id>\d+)/(?P<page>\d+)/$', views.list, name='list'), # 列表页
 ]
