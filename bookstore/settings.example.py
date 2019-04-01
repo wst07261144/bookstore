d2d10304b4b3c04cf7267653b65c5515da756155
wst07261144 <wst07261144@gmail.com>
2019-04-01 19:17:42 +0800

    ignore settings.py

--- bookstore/settings.example.py
+++ bookstore/settings.example.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$x-ubjee%ox67)gp85rt2vt7fmc9t_$i25*v1k9v#40_0mz5jb'  # 序列化session数据
+SECRET_KEY = 'xxxxx'  # 序列化session数据
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admindocs',
     'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.auth',  #  包含认证框架的核心以及默认模型
+    'django.contrib.contenttypes', # 内容类型系统，用于给模型关联许可
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -85,17 +85,17 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 # 发送邮件
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.126.com'
+EMAIL_HOST = 'smtp.163.com'
 # 126和163邮箱的SMTP端口为25； QQ邮箱使用的SMTP端口为465
 EMAIL_PORT = 25
 # 如果使用QQ邮箱发送邮件，需要开启SSL加密, 如果在aliyun上部署，也需要开启ssl加密，同时修改端口为EMAIL_PORT = 465
 # EMAIL_USE_SSL = True
 # 发送邮件的邮箱
-EMAIL_HOST_USER = 'wst07261144@163.com'
+EMAIL_HOST_USER = ''
 # 在邮箱中设置的客户端授权密码
-EMAIL_HOST_PASSWORD = '123qweasdzxc'
+EMAIL_HOST_PASSWORD = ''
 # 收件人看到的发件人
-EMAIL_FROM = 'test<wst07261144@163.com>'
+EMAIL_FROM = '<wst07261144@163.com>'
 
 # 全文检索配置
 HAYSTACK_CONNECTIONS = {
@@ -117,10 +117,10 @@ HAYSTACK_SEARCH_RESULTS_PER_PAGE = 6 # 指定搜索结果每页的条数
 MIDDLEWARE = [
     'django.contrib.admindocs.middleware.XViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', # 通过请求管理会话
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', # 将会话和用户关联
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -222,10 +222,10 @@ LOGGING = {
         },
         'default': {
             'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
+            'class': 'logging.handlers.RotatingFileHandler', # 磁盘日志文件的旋转
             'filename': '{}/Log/QWebFX_{}.log'.format(BASE_DIR, datetime.datetime.now().date()),  # 日志输出文件
             'maxBytes': 1024 * 1024 * 5,  # 文件大小
-            'backupCount': 5,  # 备份份数
+            'backupCount': 5,  # 用于指定保留的备份文件的个数。比如，如果指定为2，当上面描述的重命名过程发生时，原有的chat.log.2并不会被更名，而是被删除。
             'formatter': 'standard',  # 使用哪种formatters日志格式
         },
         'error': {
